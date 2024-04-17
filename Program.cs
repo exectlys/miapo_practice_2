ccf5c19f8610bbd49d1cddd4b9482ea838dde7ff
Мамзер Диана Сергеевна <gr621_madse@ttit.local>
2024-04-17 15:21:02 +0700

    Calc. Add function plus, minus, division, multiplication

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1;
+
+public class CalcLibrary
+{
+    public class Calc
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+        public int Minus(int a, int b)
+        {
+            return a - b;
+        }
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+        public int Mult(int a, int b)
+        {
+            return a * b;
+        }    
+    }
+   
+}
+
