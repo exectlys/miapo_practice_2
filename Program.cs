9fbd556558bdcf85beeff56134fec46ac54476c9
Адуев Никита Александрович <gr621_adnal@ttit.local>
2024-04-17 15:21:35 +0700

    New Programm.cs with deffects

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Programm
+{
+    public double a;
+    public double b;
+    
+    public double Root()
+    {
+        return Math.Pow(a, b);
+    }
+
+    public double Pow()
+    {
+        return Math.Sqrt(a);
+    }
+
+    public double Sin()
+    {
+        return Math.Cos(a);
+    }
+
+    public double Cos()
+    {
+        return Math.Sin(a);
+    }
+}
